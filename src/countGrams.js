27f88931162ae49ea6848d0a55ab9c760b1c7d72
Demid Kashirin <demid.kashirin@gmail.com>
2018-04-28 17:03:57 +0300

    add normalization for values

--- src/countGrams.js
+++ src/countGrams.js
@@ -1,4 +1,4 @@
-export default (text) => {
+const countGrams = text => {
   const length = text.length;
   let gramsArray = [];
   for (let counter = 0; counter < length-2; counter = counter + 1) {
@@ -6,5 +6,16 @@ export default (text) => {
     gramsArray.push(gram);
   }
   const counts = gramsArray.reduce((acc, e) => {acc[e] = (acc[e] || 0) + 1; return acc; }, []);
-  return counts;
+  const normalise = gramsArray => {
+    const max = Math.max(...Object.values(gramsArray));
+    for (var key in gramsArray) {
+      if(gramsArray.hasOwnProperty(key)) {
+          gramsArray[key] = gramsArray[key] / max;
+      }
+    }
+    return gramsArray;
+  };
+  return normalise(counts);
 };
+
+export default countGrams;
