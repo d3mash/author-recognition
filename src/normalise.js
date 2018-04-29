b43f72491e7fbb7c4a31a64d0e8e4ca48d70b4c7
Demid Kashirin <demid.kashirin@gmail.com>
2018-04-29 19:46:39 +0300

    fix not normalising double whitespaces

--- src/normalise.js
+++ src/normalise.js
@@ -2,17 +2,11 @@
 // get it all to lowercase, get rid of punctuation marks and so on
 const normaliseText = (text) => {
   const preformatted = text.toLowerCase();
-  const textLength = text.length;
   let output = '';
-  const isPunctuation = symbol => {
-    return symbol === '.' || symbol === ',' || symbol === '!' || symbol === '?' || symbol === ':' || symbol === ';' || symbol === '-';
-  };
-  for (let counter = 0; counter < textLength; counter = counter + 1) {
-    if (!isPunctuation(preformatted[counter])) {
-      output = output + preformatted[counter];
-    }
-  }
-  return output;
+  const regex = /[абвгдеёжзийклмнопрстуфхцчшщъыьэюя ]+/g;
+  const punctuationFree = text.match(regex).join('');
+  const trailingWhitespaceFree = punctuationFree.replace(/ +(?= )/g,'');
+  return trailingWhitespaceFree;
 };
 
 export default normaliseText;
