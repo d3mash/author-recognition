2e0934b78318e9b62ce1a1d379567e2ddcb1ab00
Demid Kashirin <demid.kashirin@gmail.com>
2018-04-30 11:49:57 +0300

    fix normalise

--- src/normalise.js
+++ src/normalise.js
@@ -1,11 +1,11 @@
 // this file is designed to normalise the input - as in
 // get it all to lowercase, get rid of punctuation marks and so on
+
 const normaliseText = (text) => {
   const preformatted = text.toLowerCase();
-  let output = '';
   const regex = /[абвгдеёжзийклмнопрстуфхцчшщъыьэюя ]+/g;
-  const punctuationFree = text.match(regex).join('');
-  const trailingWhitespaceFree = punctuationFree.replace(/ +(?= )/g,'');
+  const punctuationFree = preformatted.match(regex).join('');
+  const trailingWhitespaceFree = punctuationFree.replace(/\s\s+/g, ' ');
   return trailingWhitespaceFree;
 };
 
