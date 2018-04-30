9989a3853b296b6668f4ea9710c5b56804dcc739
Demid Kashirin <demid.kashirin@gmail.com>
2018-04-30 11:47:32 +0300

    fix infix operator spacing

--- src/countGrams.js
+++ src/countGrams.js
@@ -10,8 +10,8 @@ const sumGramsCount = (object) => {
 const countGrams = text => {
   const length = text.length;
   let gramsArray = [];
-  for (let counter = 0; counter < length-2; counter = counter + 1) {
-    let gram = text[counter]+text[counter+1]+text[counter+2];
+  for (let counter = 0; counter < length - 2; counter = counter + 1) {
+    let gram = text[counter] + text[counter + 1] + text[counter + 2];
     gramsArray.push(gram);
   }
   const counts = gramsArray.reduce((acc, e) => {acc[e] = (acc[e] || 0) + 1; return acc; }, []);
