6a32b67fbd8c5e78b5c63122cc3763351bae0478
Demid Kashirin <demid.kashirin@gmail.com>
2018-10-03 16:56:59 +0300

    fix distanceFinder

--- src/algorithms/distanceCounter.js
+++ src/algorithms/distanceCounter.js
@@ -1,7 +1,18 @@
-import countGrams from './countGrams';
-import normalise from './normalise';
+import { keys, union } from 'lodash';
+import grams from './countGrams';
 
-const getDistance = (textOne, textTwo) => {
-  const textOneGrams = countGrams(normalise(textOne));
-  const textTwoGrams = countGrams(normalise(textTwo));
+const getDistance = (text1, text2) => {
+  const grams1 = grams(text1);
+  const grams2 = grams(text2);
+  // merging keys to find insimilarities
+  const unionGrams = union(keys(grams1), keys(grams2));
+  unionGrams.reduce((distance, gram) => {
+    if (!grams1[gram]) {
+      return distance + (grams2[gram] * grams2[gram]);
+    } else if (!grams2[gram]) {
+      return distance + (grams1[gram] * grams1[gram]);
+    }
+    return distance + ((grams1[gram] - grams2[gram]) ** 2);
+  }, 0);
 };
+export default getDistance;
